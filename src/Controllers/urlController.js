d4fb9c5f321a2daad56d5eb84c95bc44097056a8
RituSharma2 <ritu76814@gmail.com>
2021-12-15 09:22:53 +0530

    caching update

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -6,7 +6,7 @@ const validUrl = require('valid-url')
 const shortenUrl = async function (req, res) {
     try {
         const baseUrl = 'http://localhost:3000'
-        const longUrl = req.body.longUrl.trim()
+        const longUrl = req.body.longUrl
 
         if (req.body && Object.keys(req.body).length == 0) {
             return res.status(400).send({ status: false, msg: "provide body " })
@@ -17,7 +17,7 @@ const shortenUrl = async function (req, res) {
         }
         const urlCode = shortid.generate()
 
-        if (validUrl.isUri(longUrl)) {
+        if (validUrl.isUri(longUrl.trim())) {
             let url = await urlModel.findOne({ longUrl: longUrl })
             if (url) {
                 res.status(200).send({status:true, message: "You have already created shortUrl for the requested URL as given below", data: url.shortUrl })
